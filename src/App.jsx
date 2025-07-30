a9779113e1f6cebf41accf373cfb945d84bc7315
AbdulGaffarDev <a.gaffar.dev@gmail.com>
2025-07-31 00:49:54 +0500

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center px-2 py-4">
+   
+    </div>
+  )
+}
+
+export default App
